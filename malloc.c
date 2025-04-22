926a487e3c4b6d5255313eec057ab0a2aa21e738
Radwan <radwanjama099@gmail.com>
2025-04-21 23:00:07 -0700

    heap viz

--- malloc.c
+++ malloc.c
@@ -9,6 +9,25 @@ typedef struct Block{
 } Block;
 Block* head = NULL; //has to be global cause we always change head in our malloc function
 
+void my_free(void* ptr){
+    if(ptr == NULL){
+        return;
+    }
+    Block* block = (Block*)ptr-1;
+    block->free = 1;
+    printf("Freed block @ %p (size:%zu)\n", (void*)block,block->size);
+
+}
+
+void print_heap(){
+    Block* curr = head;
+    while (curr!=NULL){
+        printf("Block @ %p | size : %zu | free %s\n", (void*)curr,curr->size, curr->free ? "yes":"no" );
+        curr = curr->next;
+
+    }
+}
+
 void* my_malloc(size_t size){
     Block* curr = head;
     // in this linked list each block is a node of memory in which we can free or allocate
@@ -19,6 +38,26 @@ void* my_malloc(size_t size){
         } 
         curr = curr->next;
     }
+    Block* new_block = (Block*)sbrk(sizeof(Block)+size);
+    if(new_block == (void*)-1){
+        return NULL;
+    }
+
+    new_block->size = size;
+    new_block->free = 0;
+    new_block->next = NULL;
+
+    if(head == NULL){
+        head = new_block;
+    } else{
+        curr = head;
+        while(curr->next != NULL){
+            curr = curr->next;
+        }
+        curr->next = new_block;
+    }
+    return (void*)(new_block+1);
+
 
 }
 int main(){
@@ -34,5 +73,15 @@ int main(){
     for(int i = 0;i<5;i++){
         printf("nums[%d]=%d\n",i,nums[i]);
     }
+
+    print_heap();
+
+    my_free(nums);
+
+    print_heap();
+
+
     return 0;
+
+
 }
